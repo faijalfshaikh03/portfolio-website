39f03ab74adc06beb52a9ea1c698273c861c9286
faijalfshaikh <faijalfshaikh@gmail.com>
2026-01-15 14:12:16 +0530

    Imported dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Education from './components/Education/Education'
+import Experience from './components/Experience/Experience'
+import Navbar from './components/Navbar/Navbar'
+import Skills from './components/Skills/Skills'
+import Work from './components/Work/Work'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
